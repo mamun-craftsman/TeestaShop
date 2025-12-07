9081c7ab235babfcfe43ca61562bcb5a276cc904
mamun-craftsman <200101.cse@student.just.edu.bd>
2025-12-07 14:16:07 +0600

    ggg

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ if (file_exists($maintenance = __DIR__.'/core/storage/framework/maintenance.php'
 }
 
 require __DIR__.'/core/vendor/autoload.php';
-
 $app = require_once __DIR__.'/core/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
